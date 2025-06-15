46d737617749d87c6c358ac7fce86d6d50649b75
Alex89 <alex.wangzhe0809@gmail.com>
2025-06-15 20:53:05 +0200

    1st version

---
+++ frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Button, Container, Stack, Text, Toast } from "@chakra-ui/react";
+import Navbar from "./components/Navbar";
+import UserGrid from "./components/UserGrid";
+
+export const BASE_URL = "http://127.0.0.1:5000/api";
+
+const App = () => {
+  const [users, setUsers] = useState([]);
+
+  return (
+    <Stack minH={"100vh"}>
+      <Navbar setUsers={setUsers} />
+      <Container maxW={"1200px"} my={4}>
+        <Text
+          fontSize={{ base: "3xl", md: "50" }}
+          fontWeight={"bold"}
+          letterSpacing={"2px"}
+          textTransform={"uppercase"}
+          textAlign={"center"}
+          mb={8}
+        >
+          <Text as={"span"} bgGradient={"linear(to-r, cyan.400, blue.500)"} bgClip={"text"}>
+            My Besties
+          </Text>
+        </Text>
+        <UserGrid users={users} setUsers={setUsers} />
+      </Container>
+    </Stack>
+  );
+};
+
+export default App;
